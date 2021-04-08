d2affc65795772f19add32c154e4844b621e990c
jasminecahuana <jasminecahuana@gmail.com>
2021-04-07 23:06:16 -0400

    implemented DataTable. Attemping to make DataTable code generic.

--- getCOVID.php
+++ getCOVID.php
@@ -1,25 +1,31 @@
 <?php 
     Include 'db_connection.php';
     $con = OpenCon();
+    $q = $_REQUEST["dataset"];
 
-    $sql = "SELECT state, covid_19_deaths FROM iidts_prod.disease_covid19_cdc_api_t where age_group='All Ages' and sex='All Sexes' and data_as_of='2021-03-10' LIMIT 54;";
-    
-    $result = mysqli_query($con, $sql);
-    echo '<table ref={tableRef}>
-            <thead>
-                <tr>
-                    <th>State</th>
-                    <th>Deaths</th>
-                </tr>
-            </thead>
-            <tbody>';
-    while($row = mysqli_fetch_assoc($result)) {
-        
-        echo "<tr><td>". $row["state"]. "</td><td>" . $row["covid_19_deaths"]."</tr></td>";
+    if ($q == "COVID-19"){
+        $sql = "SELECT state, covid_19_deaths FROM iidts_prod.disease_covid19_cdc_api_t where age_group='All Ages' and sex='All Sexes' and data_as_of='2021-03-10' LIMIT 54;";
+            
+    }else{
+        echo("error");
     }
-    echo '</tbody>
-    </table>
-    ';
+    switch($q){
+        case "COVID-19":
+            $sql = "SELECT state, covid_19_deaths FROM iidts_prod.disease_covid19_cdc_api_t where age_group='All Ages' and sex='All Sexes' and data_as_of='2021-03-10' LIMIT 54;";
+            break;
+        case "Anaplasmosis":
+            $sql = "SELECT * FROM iidts_prod.disease_anaplasmosis_state_t limit 51;";
+        default:
+            echo("Error")
+    }
+    $result = mysqli_query($con, $sql);
+
+    $rows = array();
+    while($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+
+    echo json_encode($rows);
     mysqli_close($con);
 
 ?>
